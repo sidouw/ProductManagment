ef5a2da7a33fcb077a870e99dfd020e40b75e7eb
sidouw <chaouadi5@gmail.com>
2023-09-21 02:19:29 +0100

    Front End Setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <h1>Product Managment</h1>
+    </ThemeProvider>
+  );
+}
+
+export default App;
